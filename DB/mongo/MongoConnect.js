2d902fcea7b785b1beb4bd8481d868a5855b7b67
Slibul <81206047+Slibul@users.noreply.github.com>
2026-05-26 13:23:48 +0900

    .evn파일을 제대로 못 읽어들이는 문제가 발생한것으로 추정되므로 나중에 확인

--- DB/mongo/MongoConnect.js
+++ DB/mongo/MongoConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectMongo = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/skul_realtime");
+        await mongoose.connect(process.env.MONGO_URI || "mongodb://root:mysql123@ac-64s16x5-shard-00-00.r0yfenk.mongodb.net:27017,ac-64s16x5-shard-00-01.r0yfenk.mongodb.net:27017,ac-64s16x5-shard-00-02.r0yfenk.mongodb.net:27017/gamedata?ssl=true&replicaSet=atlas-uzj4dr-shard-0&authSource=admin&appName=Cluster0");
         console.log("[MongoDB] 연결 성공");
     } catch (err) {
         console.error("[MongoDB] 연결 실패:", err.message);
